707a2d083cd95d482dbd4eb72f3c7893f1ed83cd
Roger Stringer <freekrai@me.com>
2020-01-18 09:47:28 -0800

    finish button block

--- src/components/CoreButtonBlock/CoreButtonBlock.js
+++ src/components/CoreButtonBlock/CoreButtonBlock.js
@@ -2,60 +2,78 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CoreButtonBlock = props => {
-	const { url, title, text, textColor, CustomTextColor, CustomBackgroundColor } = props.attributes;
+	const {
+		align,
+		backgroundColor,
+		className,
+		customBackgroundColor,
+		customTextColor,
+		text,
+		textColor,
+		title,
+		url
+	} = props.attributes;
+
+	const styles = {};
+	const classes = ['wp-block-button'];
+
+	if (className) {
+		classes.push(className);
+	}
+
+	if (align) {
+		styles.textAlign = align;
+	}
+	if (backgroundColor) {
+		styles.backgroundColor = backgroundColor;
+	}else if (customBackgroundColor) {
+		styles.backgroundColor = customBackgroundColor;
+	}
+
+	if( textColor ){
+		styles.color = textColor;
+	}else if( customTextColor ){
+		styles.color = customTextColor;
+	}
 
 	return (
-        <div className="wp-block-button">
-            <a className="wp-block-button__link" href={url}>{text}</a>
+        <div
+					{...classes.length > 0 && { className: classes.join(' ') }}
+					style={styles}
+				>
+            <a className="wp-block-button__link" href={url}>
+							{text}
+						</a>
         </div>
 	);
 };
 
 CoreButtonBlock.propTypes = {
 	attributes: PropTypes.shape({
-        url: PropTypes.string,
-        title: PropTypes.string,
-        text: PropTypes.string,
-        textColor: PropTypes.string,
-        CustomTextColor: PropTypes.string,
-        CustomBackgroundColor: PropTypes.string
+			align: PropTypes.string,
+			backgroundColor: PropTypes.string,
+			className: PropTypes.string,
+			customBackgroundColor: PropTypes.string,
+			customTextColor: PropTypes.string,
+			text: PropTypes.string,
+			textColor: PropTypes.string,
+			title: PropTypes.string,
+			url: PropTypes.string
 	}),
 };
 
 CoreButtonBlock.defaultProps = {
 	attributes: {
-        url: null,
-        title: null,
-        text: null, 
-        textColor: null,
-        CustomTextColor: null,
-        CustomBackgroundColor: null
+		align: null,
+		backgroundColor: null,
+		className: null,
+		customBackgroundColor: null,
+		customTextColor: null,
+		text: null,
+		textColor: null,
+		title: null,
+		url: null
 	},
 };
 
 export default CoreButtonBlock;
-
-/*
-<div class="wp-block-button"><a class="wp-block-button__link" href="https://rogerstringer.com/contact/">Contact me about writing for you</a></div>
-
-     __typename
-      align
-      backgroundColor
-      className
-      customBackgroundColor
-      customTextColor
-      text 
-      textColor
-      title  
-      url
-
-          url
-          title
-          textColor
-          customTextColor
-          customBackgroundColor
-          text
-
-
-
-*/
